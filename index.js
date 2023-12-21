23734c9a080121762bb2d66760604a4515c4c3ad
Vaishnavi <Vaishnavisoundararajan5@gmail.com>
2023-12-21 22:39:06 +0530

    Dice Challenge

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 var randomNumber1= Math.floor((Math.random()*6)+1); //1-6
 var randomNumber2= Math.floor((Math.random()*6)+1);
 var randomDiceImage1= "./assets/images/dice"+[randomNumber1]+".png";
